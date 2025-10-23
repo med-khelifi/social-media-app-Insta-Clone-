ca7f24e73c63affd0616eccd7cd6699beee9872e
Mohamed khelifi <khelifim440@gmail.com>
2025-10-23 21:13:47 +0100

    add upload post with supabase

--- lib/core/supabase/supabase_credential.dart
+++
@@ -1,5 +0,0 @@
-class SupabaseCredential {
-  static final url = "https://otdgzwccdfguwtygpuet.supabase.co";
-  static final anonKey =
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im90ZGd6d2NjZGZndXd0eWdwdWV0Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2MTA3MjI5NCwiZXhwIjoyMDc2NjQ4Mjk0fQ.nlYWaoAUGGIISmvEjDyfy_-f3n-uMxeXFgaQn-Be2DY";
-}
